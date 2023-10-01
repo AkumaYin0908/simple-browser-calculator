b2bae8fe23b925adb074730967700390d78e6e0b
AkumaYin0908 <buzterblaze0908@gmail.com>
2023-10-01 19:36:06 +0800

    upload project to Github

---
+++ README.md
@@ -0,0 +1,13 @@
+
+A Simple Browser Calculator built in basic HTML,CSS and JavaScript
+
+
+FullScreen(Width: 1920px)
+![FullScreen](screenshot/calculator-fullscreen-1920-1080.png?raw=true)
+
+Width: 1896px and below
+![FullScreen](screenshot/calculator-width-1896-and-below.png?raw=true)
+
+Width: 912px and below
+![FullScreen](screenshot/calculator-width-912px-and-below.png?raw=true)
+
---
+++ index.js
@@ -0,0 +1,162 @@
+    const numbers=[];
+    const operators=[];
+    let isClear=true;
+
+    const txtInput=document.getElementById("calculator-text-box");
+
+    document.querySelectorAll("button").forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        let buttonId=btn.id;
+        buttonAnimation(buttonId);
+        fillTextInput(buttonId);
+    })
+    });
+
+
+    document.addEventListener("keydown",handleKeyPress);
+
+    function handleKeyPress(event){
+        let key=event.key;
+        if(key==="Enter"){
+             key="=";
+        }else if(key==="Backspace"){
+            key="delete";
+        }
+        buttonAnimation(key);
+        fillTextInput(key);
+    }
+    function isButton(currentId){
+        const regexPattern=/[0-9+\-/*=.]/;
+        return currentId.match(regexPattern) || ["sign","clear","delete"].includes(currentId);
+    }
+
+    function buttonAnimation(currentButton){
+        if(isButton(currentButton)){
+            const element=document.getElementById(currentButton);
+            element.classList.add("pressed");
+
+            setTimeout(()=>{
+            element.classList.remove("pressed");
+
+            },100);
+            
+        }else{
+            console.log(currentButton);
+        }
+    }
+
+    function clearCalculator(){
+        txtInput.value="";
+        numbers.length=0;
+        operators.length=0;
+        isClear=true;
+    }
+
+    function handleOperator(currentButton){
+
+        if(txtInput.value.trim() === ""){
+            clearCalculator();
+
+        }else{
+            if(numbers.length<1 && operators.length<1){
+                operators.push(currentButton);
+                numbers.push(txtInput.value);
+                txtInput.value="";
+            }else{
+                numbers.push(txtInput.value);
+                txtInput.value = calculate(numbers,operators).toLocaleString();
+                numbers.length=0;
+                operators.length=0;
+                operators.push(currentButton);
+                numbers.push(txtInput.value);
+                isClear=false;
+            }
+        }
+        
+    }
+
+    function handleEquals(){
+        numbers.push(txtInput.value);
+        console.log(numbers);
+        console.log(operators);
+        txtInput.value = calculate(numbers,operators).toLocaleString();
+        numbers.length=0;
+        operators.length=0;
+    }
+
+    function fillTextInput(currentButton){
+        
+        if(isButton(currentButton)){
+            switch(currentButton){
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                   handleOperator(currentButton);
+                break;
+
+                case "=":
+                    handleEquals(); 
+                break;
+                case "clear":
+                    clearCalculator();
+                break;
+                case "delete":
+                    txtInput.value=txtInput.value.slice(0,-1);
+                break;
+                case "sign":
+                    txtInput.value=parseInt(-txtInput.value);
+                    break;
+                default:
+                   if(!isClear || txtInput.value == 0){
+                        txtInput.value="";
+                        isClear=true;
+                   }
+                    txtInput.value+=currentButton;
+                break;
+            }
+
+        }
+        
+    }
+
+function calculate(numbers,operators){
+    let result=parseInt(numbers[0].replace(',',''));
+    try{
+        for(let index=0; index<operators.length; index++){
+            let operator = operators[index];
+            let operand = parseInt(numbers[index + 1].replace(',',''));
+            switch(operator){
+                case "+":
+                    result += operand;
+                break;
+
+                case "-":
+                    result -= operand;
+                break;
+
+                case "*":
+                    result *= operand;
+                break;
+
+                case "/":
+                    result /= operand;
+                break;
+
+                default:
+                    result = 0;
+                break;
+            }
+           
+        }
+
+    }catch(error){
+    alert("Error calculating result");
+    }
+    return result;
+}
+   
+
+    
+
+
